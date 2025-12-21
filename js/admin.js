09de1cab372458fd7a91555d4200bfba8d27edfa
mahamuds5545-afk <mahamuds5545@gmail.com>
2025-12-21 10:07:59 +0600

    Add files via upload

---
+++ js/admin.js
@@ -0,0 +1,12 @@
+// Admin functions
+console.log("Admin JS loaded!");
+
+function createTournament() {
+    alert("Create tournament function");
+}
+
+function deleteTournament(id) {
+    if(confirm("Delete this tournament?")) {
+        alert("Deleted!");
+    }
+}
